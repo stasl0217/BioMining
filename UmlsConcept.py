382533fc9d7467811f695dac7102fcbdbf979ab5
Shirley Chen <sinternoal@msn.com>
2017-08-03 16:36:09 -0700

    UmlsConcept

---
+++ UmlsConcept.py
@@ -0,0 +1,7 @@
+class UmlsConcept:
+    def __init__(self,CUI, typeID, begin, end, preferred_text):
+        self.CUI=CUI
+        self.typeID=typeID
+        self.begin=begin
+        self.end=end
+        self.preferred_text=preferred_text
