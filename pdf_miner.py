5fca7fd75ac6465aff5e805e74b254c7f25706c3
Shirley Chen <sinternoal@msn.com>
2017-08-08 21:40:17 -0700

    add functions

--- pdf_miner.py
+++ pdf_miner.py
@@ -9,6 +9,7 @@ from pdfminer.layout import *
 from pdfminer.converter import PDFPageAggregator
 from os.path import join
 import re
+import sys
 
 
 def mining_pdf(pdf_dir, txt_dir, filename):
@@ -47,9 +48,71 @@ def mining_pdf(pdf_dir, txt_dir, filename):
 
 
 def text_rep(in_dir, file, out_dir):
-    with open(join(in_dir,file), 'r') as f:
+    with open(join(in_dir, file), 'r') as f:
         str = f.read()
+
+        # get rid of the exceptional \n
         pattern = re.compile(r'([^\n])\n([^\n])')
         result = pattern.sub(r'\1\2', str)  # 剔除换行符
+
+        # get rid of invalid XML characters
+
         with open(join(out_dir, file), 'w') as fout:
             fout.write(result)
+
+
+
+def invalid_xml_remove(c):
+    """
+    Source: https://gist.github.com/lawlesst/4110923
+    """
+    # http://stackoverflow.com/questions/1707890/fast-way-to-filter-illegal-xml-unicode-chars-in-python
+    illegal_unichrs = [(0x00, 0x08), (0x0B, 0x1F), (0x7F, 0x84), (0x86, 0x9F),
+                       (0xD800, 0xDFFF), (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF),
+                       (0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF), (0x3FFFE, 0x3FFFF),
+                       (0x4FFFE, 0x4FFFF), (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF),
+                       (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF), (0x9FFFE, 0x9FFFF),
+                       (0xAFFFE, 0xAFFFF), (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF),
+                       (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF), (0xFFFFE, 0xFFFFF),
+                       (0x10FFFE, 0x10FFFF)]
+
+    illegal_ranges = ["%s-%s" % (unichr(low), unichr(high))
+                      for (low, high) in illegal_unichrs
+                      if low < sys.maxunicode]
+
+    illegal_xml_re = re.compile(u'[%s]' % u''.join(illegal_ranges))
+    if illegal_xml_re.search(c) is not None:
+        # Replace with space
+        return ' '
+    else:
+        return c
+
+
+def clean_char(char):
+    """
+    Source: https://gist.github.com/lawlesst/4110923
+    """
+    """
+    Function for remove invalid XML characters from
+    incoming data.
+    """
+    # Get rid of the ctrl characters first.
+    # http://stackoverflow.com/questions/1833873/python-regex-escape-characters
+    char = re.sub('\x1b[^m]*m', '', char)
+    # Clean up invalid xml
+    char = invalid_xml_remove(char)
+    replacements = [
+        (u'\u201c', '\"'),
+        (u'\u201d', '\"'),
+        (u"\u001B", ' '),  # http://www.fileformat.info/info/unicode/char/1b/index.htm
+        (u"\u0019", ' '),  # http://www.fileformat.info/info/unicode/char/19/index.htm
+        (u"\u0016", ' '),  # http://www.fileformat.info/info/unicode/char/16/index.htm
+        (u"\u001C", ' '),  # http://www.fileformat.info/info/unicode/char/1c/index.htm
+        (u"\u0003", ' '),  # http://www.utf8-chartable.de/unicode-utf8-table.pl?utf8=0x
+        (u"\u000C", ' ')
+    ]
+    for rep, new_char in replacements:
+        if char == rep:
+            # print ord(char), char.encode('ascii', 'ignore')
+            return new_char
+    return char
