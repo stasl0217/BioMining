e50c3787d612c2523ebbcf963eab5797b4fbf7e3
Shirley Chen <sinternoal@msn.com>
2017-08-08 21:39:58 -0700

    update dir. next: remove invalid XML char

--- pdf2txt.py
+++ pdf2txt.py
@@ -5,14 +5,14 @@ from os import listdir
 from os.path import isfile, join
 import pdf_miner
 
-pdf_dir= r'C:\Users\sinte\LULU\lab\biomining\papers'
+pdf_dir= r'C:\Users\sinte\LULU\lab\biomining\unprocessed_pdf'
 files=[f for f in listdir(pdf_dir) if isfile(join(pdf_dir, f))]
-txt_dir1=r'C:\Users\sinte\LULU\lab\biomining\txt_temp'
+txt_dir1=r'C:\Users\sinte\LULU\lab\biomining\unprocessed_txttmp'
 for f in files:
     pdf_miner.mining_pdf(pdf_dir,txt_dir1,f)
 
 txt_files=[f for f in listdir(txt_dir1) if isfile(join(txt_dir1, f))]
-txt_dir2=r'C:\Users\sinte\LULU\lab\biomining\txt'
+txt_dir2=r'C:\Users\sinte\LULU\lab\biomining\unprocessed_txt'
 for f in txt_files:
     pdf_miner.text_rep(txt_dir1,f,txt_dir2)
 
